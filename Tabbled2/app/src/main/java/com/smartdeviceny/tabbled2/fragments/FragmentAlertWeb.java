2d812c2e9aa527485091e1b4b810f9e2f63023cd
Anil Sarma <asarma@strat11.com>
2018-07-26 21:31:08 -0400

    fix refresh

--- Tabbled2/app/src/main/java/com/smartdeviceny/tabbled2/fragments/FragmentAlertWeb.java
+++ Tabbled2/app/src/main/java/com/smartdeviceny/tabbled2/fragments/FragmentAlertWeb.java
@@ -23,8 +23,9 @@ public class FragmentAlertWeb extends Fragment implements ServiceConnected {
         try {
             View view = inflater.inflate(R.layout.fragment_alert_web, container, false);
             WebView webview = (WebView)view.findViewById(R.id.web_view);
-            webview.getSettings().setJavaScriptEnabled(true);
+            //webview.getSettings().setJavaScriptEnabled(true);
             webview.getSettings().setSupportZoom(true);
+            webview.getSettings().setBuiltInZoomControls(true);
             return view;
         }catch (Exception e) {
             return null;
